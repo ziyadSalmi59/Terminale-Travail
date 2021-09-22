8677e615c3202fc6fe66fc663328f0210e6d519b
ziyad_slm <70703730+ziyadslm@users.noreply.github.com>
2021-09-22 15:47:12 +0200

    Add files via upload

---
+++ TP base python.py
@@ -0,0 +1,43 @@
+exercice 4:
+    1- idcmd, idclient/produit
+    2-3,21+ 2x10,57 + 3,21 + 2 + 4x53,25 = 242.56
+    3- Alain terieur, gilles héjone, hélène de troie
+    4- le produit 1
+    
+exercice 5:
+    PARTIE 1:
+        1-id, titre, realisateur,ann-sortie, note_sur_10
+        2-int, str, str, int, int
+        3-l idd peut jouer le role de lid ainsi que le titre peuvent faire office de clef primaire
+        
+    PARTIE 2:
+        1- nom: ridley, premom: scott, jour_de_naissance: 30, mois_de_naissance: novembre, année_de_naissance: 1937
+        2- nom: David, premom: Lynch, jour_de_naissance: 20, mois_de_naissance: janvier, année_de_naissance: 1946
+        3- nom: Stanley, premom: Kubrick, jour_de_naissance: 26, mois_de_naissance: juillet, année_de_naissance: 1928
+        
+Exercice 7:
+    
+    
+nom | prenom | classe | numero_etudiant
+------------------------------------------
+jean  paul     6e      0
+
+
+
+Matière_enseigné | numero_etudiant | cursus_eleve
+-------------------------------------------------
+histoire           0
+                   1
+                    
+
+
+
+
+svt
+musique
+math
+    
+    
+        
+        
+    
